a1f03d53a0a585ae57085ea765f73f453f11f206
15104902830 <2021004725@qq.com>
2022-12-28 17:31:27 +0800

    初始化

---
+++ node_modules/@mniui/components/icon/src/icon.ts
@@ -0,0 +1,6 @@
+import type { ExtractPropTypes, PropType } from "vue";
+
+export const iconProps = {size: [Number, String] as PropType<number | string>,color: String,
+} as const;
+
+export type IconProps = ExtractPropTypes<typeof iconProps>; 
